25de5e8b800c6f0932ef93c171e0dc31bdf78e73
Srinithi23 <78066890+Srinithi23@users.noreply.github.com>
2021-01-27 22:44:51 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hard_1
+{
+    class Employee
+    {
+        int id, age;
+        string name;
+        double salary;
+
+        static Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
+
+        public Employee()
+        {
+        }
+
+        public Employee(int id, int age, string name, double salary)
+        {
+            this.id = id;
+            this.age = age;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void TakeEmployeeDetailsFromUser()
+        {
+            try
+            {
+                Console.WriteLine("Please enter the employee ID");
+                id = int.Parse(Console.ReadLine());
+                if (employees.ContainsKey(id))
+                {
+                    Console.WriteLine("This ID has been already added !!! Use a new ID. ");
+                    TakeEmployeeDetailsFromUser();
+                    return;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("ID must be a number !!!");
+                TakeEmployeeDetailsFromUser();
+                return;
+            }
+
+            Console.WriteLine("Please enter the employee name");
+            name = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine("Please enter the employee age");
+                age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Age must be a number !!!");
+                TakeEmployeeDetailsFromUser();
+                return;
+            }
+
+            try
+            {
+                Console.WriteLine("Please enter the employee salary");
+                salary = Double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Salary must be a number !!!");
+                TakeEmployeeDetailsFromUser();
+                return;
+            }
+
+            try
+            {
+                employees.Add(id, new Employee(id, age, name, salary));
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("ID value cannot be NULL !!!!");
+                TakeEmployeeDetailsFromUser();
+                return;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("This ID has been already added !!! Use a new ID. ");
+                TakeEmployeeDetailsFromUser();
+                return;
+            }
+
+        }
+
+        public static void ModifyEmployeeDetails()
+        {
+            int id = -1;
+            try
+            {
+                Console.WriteLine("Please enter the employee ID : ");
+                id = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("ID must be an Integer !!!");
+                ModifyEmployeeDetails();
+                return;
+            }
+
+            Console.WriteLine("The employee details:");
+
+            Employee _empModify = employees[id];
+            if (_empModify != null)
+            {
+
+                Console.WriteLine(_empModify.ToString());
+
+                Console.WriteLine("Please enter the updated employee details : ");
+                Console.WriteLine("Please enter the employee name : ");
+                _empModify.name = Console.ReadLine();
+
+                try
+                {
+                    Console.WriteLine("Please enter the employee age : ");
+                    _empModify.age = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Age must be a number !!!");
+                    ModifyEmployeeDetails();
+                    return;
+                }
+
+                try
+                {
+                    Console.WriteLine("Please enter the employee salary");
+                    _empModify.salary = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Salary must be a number !!!");
+                    ModifyEmployeeDetails();
+                    return;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Employee not found in this ID !!!");
+                ModifyEmployeeDetails();
+                return;
+            }
+        }
+
+        public static void PrintEmployeeDetails()
+        {
+            foreach (KeyValuePair<int, Employee> pair in employees)
+            {
+                Console.WriteLine(pair.Value.ToString());
+                Console.WriteLine("------------------------");
+            }
+        }
+
+        public static void PrintEmployeeByID()
+        {
+            int id = -1;
+            try
+            {
+                Console.WriteLine("Please enter the employee ID : ");
+                id = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("ID must be an Integer !!!");
+                ModifyEmployeeDetails();
+            }
+
+            if (employees.ContainsKey(id))
+            {
+                Console.WriteLine("The employee details:");
+                Employee emp = employees[id];
+                Console.WriteLine(emp.ToString());
+            }
+            else
+            {
+                Console.WriteLine("No Employee is not found in this ID !!!!");
+                PrintEmployeeByID();
+                return;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return "\nEmployee ID : " + id + "\nName : " + name + "\nAge : " + age + "\nSalary : " + salary + "\n";
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int Age { get => age; set => age = value; }
+        public string Name { get => name; set => name = value; }
+        public double Salary { get => salary; set => salary = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hard_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DisplayMenu();
+
+            Console.ReadKey();
+        }
+
+        public static void DisplayMenu()
+        {
+            while (true)
+            {
+                int option = 0;
+                Console.WriteLine("Please enter the option : ");
+                try
+                {
+                    Console.WriteLine("\n1. Add an employee \n2. Modify an employee detail \n3. Print all employee's details \n4. Print an employee detail \n5. Exit");
+                    option = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Input !!! Enter a number from  1 to 5  ");
+                }
+
+                Employee emp = new Employee();
+                switch (option)
+                {
+                    case 1:
+                        int check = 1;
+                        while (check == 1)
+                        {
+                            emp.TakeEmployeeDetailsFromUser();
+                            try
+                            {
+                                Console.WriteLine("To continue entering employee details enter 1, to exit enter 0");
+                                check = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Enter Either 1 or 0 !!!");
+                            }
+                        }
+                        break;
+                    case 2:
+                        Employee.ModifyEmployeeDetails();
+                        break;
+                    case 3:
+                        Employee.PrintEmployeeDetails();
+                        break;
+                    case 4:
+                        Employee.PrintEmployeeByID();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Not an Valid Option !!! Enter an option from 1 to 5 ");
+                        break;
+
+                }
+            }
+        }
+
+    }
+}
